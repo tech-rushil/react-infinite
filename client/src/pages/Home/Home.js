92db976532f0fdbd5414b52f347797ca32839b9b
Rushil Gupta <rushil.rushil.gupta@gmail.com>
2021-11-03 22:57:37 +0530

    infinite scroll init

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,43 +1,107 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 
-class Home extends Component {
-    render() {
-        return (
-            <>
-                <div
-                    className="hero-img team-banner bg-image"
-                    style={{
-                        backgroundImage: `url("https://storage.googleapis.com/virtusbox/welcome-pirate.svg")`
-                    }}
-                >
-                </div>
+const allData = new Array(50).fill(0).map((_val, i) => i + 1);
 
-                <style jsx={'true'}>
-                    {`
-                    #root {
-                        margin: 0;
-                        background: #FFD15C;
-                    }
+const Home = () => {
+    const [loading, setloading] = useState(false);
+    const [element, setElement] = React.useState(null);
+    const [data, setdata] = useState(allData.slice(0, 10));
+    const [page, setpage] = useState(1);
 
-                    .hero-img {
-                        width: 60vw;
-                        height: 100vh;
-                        margin: 0 auto;
-                    }
+    // Load more
+    const loadMoreData = () => {
+        const currentPage = page + 1;
+        const currentData = [...data, ...allData.slice(data.length, currentPage * 10)];
+        setdata(currentData);
+        setpage(currentPage);
+    };
 
-                    .hero-img::after {
-                        content: "";
-                        width: 100%;
-                        height: 100%;
-                        background: #FFD15C;
-                        position: absolute;
-                        mix-blend-mode: darken;
-                    }
-                    `}
-                </style>
-            </>
-        );
-    }
-}
+    const loader = React.useRef(loadMoreData);
+
+    useEffect(() => {
+        loader.current = loadMoreData;
+    }, [loadMoreData]);
+
+    const observer = useRef(
+        new IntersectionObserver(
+            (entries) => {
+                const first = entries[0];
+                if (first.isIntersecting) {
+                    console.log("heelo instersecting");
+                    // loadMoreData();
+                    loader.current();
+                }
+            },
+            { threshold: 1 }
+        )
+    );
+
+    // Initialize Observer
+    useEffect(() => {
+        const currentElement = element;
+        const currentObserver = observer.current;
+
+        if (currentElement) {
+            currentObserver.observe(currentElement);
+        }
+
+        return () => {
+            if (currentElement) {
+                currentObserver.unobserve(currentElement);
+            }
+        };
+    }, [element]);
+
+    return (
+        <>
+            <div className="lr-pad-d lr-pad-m">
+                <ul>
+                    {data.map((row) => (
+                        <li key={row} style={{ background: "orange" }}>
+                            {row}
+                        </li>
+                    ))}
+                    {loading && <li>Loading...</li>}
+                    {!loading && (
+                        <li ref={setElement} style={{ background: "red" }}>
+                            LOAD MORE
+                        </li>
+                    )}
+                </ul>
+            </div>
+            <style jsx={"true"}>{`
+                body {
+                    font-size: 16px;
+                    padding: 1rem;
+                }
+
+                ul {
+                    margin: 0 auto;
+                    max-width: 600px;
+                    list-style: none;
+                }
+
+                li {
+                    padding: 1rem;
+                    margin: 1rem;
+                    min-height: 100px;
+                }
+
+                button {
+                    border-radius: 0px;
+                    background: #bbded6;
+                    border: none;
+                    cursor: pointer;
+                    padding: 1rem;
+                }
+
+                span {
+                    display: block;
+                    padding: 1rem 0px;
+                }
+            `}</style>
+        </>
+    );
+};
 
 export default Home;
